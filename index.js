dc1af0bd0fe4953eb713194ba9ee000e0b9eb8ce
jdfs2110 <117124266+jdfs2110@users.noreply.github.com>
2024-02-09 23:58:45 +0100

    mega mandarina

---
+++ index.js
@@ -0,0 +1,15 @@
+$(() => {
+  $('a').on('click', function () {
+    $('.square').fadeOut(1100);
+    $('a').hide(1000);
+    $('h1').animate({
+      left: '7',
+      top: '7',
+      fontSize: '3em',
+      fontSize: '5vh'
+    }, 1100, function () {
+      // Animation complete.
+      window.location.href = 'skins2.html';
+    });
+  })
+})
--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const swup = new Swup();
-
-    document.addEventListener('swup:popstate', function () {
-        location.reload();
-    });
-});
+$(() => {
+  $('header a').on('click', function () {
+    $('main, header').animate({
+      opacity: 0.75,
+      opacity: 0.5,
+      opacity: 0.25,
+      opacity: 0
+    }, 1100)
+    let viewportHeight = $(window).height();
+    let viewportWidth = $(window).width();
+    let h1Height = $('h1').outerHeight();
+    let h1Width = $('h1').outerWidth();
+    let topOffset = 65; // Change this value as needed
+    let leftOffset = 65; // Change this value as needed
+    let topPosition = ((viewportHeight / 2) - (h1Height / 2)) - topOffset;
+    let leftPosition = ((viewportWidth / 2) - (h1Width / 2)) - leftOffset;
+    $('h1').animate({
+      left: leftPosition + 'px',
+      top: topPosition + 'px',
+      fontSize: '3em',
+      fontSize: '5vh',
+      fontSize: '10vh'
+    }, 1100, function () {
+      // Animation complete.
+      window.location.href = '/';
+    })
+  })
+})
