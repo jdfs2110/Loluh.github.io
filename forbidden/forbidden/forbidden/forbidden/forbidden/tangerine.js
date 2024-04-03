6d53c20ccf2edd69b680798a6444eee2b728229d
FranDAW1 <1922925@alu.murciaeduca.es>
2024-04-03 13:00:58 +0200

    help

--- forbidden/forbidden/forbidden/forbidden/forbidden/tangerine.js
+++ forbidden/forbidden/forbidden/forbidden/forbidden/tangerine.js
@@ -1,14 +1,10 @@
-// Number of images to load
+
 var numImages = 100;
 
-// Loop to create and append images
+
 for (var i = 0; i < numImages; i++) {
-  // Create new img element
+
   var img = document.createElement('img');
-  
-  // Set the source of the image
   img.src = '/media/images/tangerine.png';
-  
-  // Append the image to the body
   document.body.appendChild(img);
 }
